fefa57f4ed366cfa2f70f3bec9d3e5512dc4baf0
BinhSilver <nguyenphamthanhbinh02@gmail.com>
2025-06-04 00:53:21 +0700

    BTVN_LAB

---
+++ src/test/java/binhnpt/AccountServiceTest.java
@@ -0,0 +1,18 @@
+package binhnpt;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import binhnpt.example.AccountService;
+
+
+public class AccountServiceTest {
+    AccountService service = new AccountService();
+
+    @ParameterizedTest(name = "[{index}] username={0}, password={1}, email={2}, expected={3}")
+    @CsvFileSource(resources = "/test-data.csv", numLinesToSkip = 1)
+    void testRegisterAccount(String username, String password, String email, boolean expected) {
+        boolean actual = service.registerAccount(username, password, email);
+        assertEquals(expected, actual);
+    }
+}
